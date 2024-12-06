b7862c3f935747f6b10b7a685e7713ebfd433bb3
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-06 13:38:12 -0500

    Skip the slow stuff

--- 2024/src/utils.py
+++ 2024/src/utils.py
@@ -1,3 +1,13 @@
+import argparse
+
+from functools import wraps
+
+parser = argparse.ArgumentParser(prog="Advent of Code 2024")
+parser.add_argument("solution", type=int, nargs="?")
+parser.add_argument("--no-skip", type=str, nargs="*")
+
+cliargs = parser.parse_args()
+
 class Color:
     RED = 31
     GREEN = 32
@@ -66,3 +76,16 @@ class Position2D(Vec2):
 
 def colorize(text, color_code):
     return f"\033[{color_code}m{text}\033[0m"
+
+def skippable(ID):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if cliargs.no_skip is None or (len(cliargs.no_skip) > 0 and ID not in cliargs.no_skip):
+                return f"< Skipped >\tPass --no-skip {ID} to run."
+
+            return func(*args, **kwargs)
+        
+        return wrapper
+    
+    return decorator
