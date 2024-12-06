ad2ec7395ded6a575d5fb244fc0c18c0c0f575a1
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-06 01:24:30 -0500

    Naive 6p2 Python

--- 2024/src/prompt.py
+++ 2024/src/prompt.py
@@ -1,13 +1,18 @@
 class Prompt:
+    TEST_FILENAME = "test.txt"
+    PROMPT_FILENAME = "prompt.txt"
+    SOLUTION_SUFFIX = "solution.py"
+
     def read(solution_path, test=False):
-        prompt_file = "test.txt" if test else "prompt.txt"
-        f = open(solution_path.replace("solution.py", prompt_file), "r")
-        return f.read()
+        return Prompt.file(solution_path, test)
 
     def read_to_list(solution_path, test=False):
-        prompt_file = "test.txt" if test else "prompt.txt"
-        f = open(solution_path.replace("solution.py", prompt_file), "r")
-        string = f.read()
-        array = string.split("\n")
+        return Prompt.file(solution_path, test).split("\n")
 
-        return array
+    def read_to_grid(solution_path, test=False):
+        return [[c for c in row] for row in Prompt.file(solution_path, test).split("\n")]
+
+    def file(solution_path, test):
+        prompt_file = Prompt.TEST_FILENAME if test else Prompt.PROMPT_FILENAME
+        with open(solution_path.replace(Prompt.SOLUTION_SUFFIX, prompt_file), "r") as f:
+            return f.read()
