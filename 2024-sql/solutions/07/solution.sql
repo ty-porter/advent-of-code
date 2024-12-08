0587906dd6a101ab0625d33772105c925b184db0
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-07 23:40:51 -0500

    7p2 SQL

--- 2024-sql/solutions/07/solution.sql
+++ 2024-sql/solutions/07/solution.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS equations;
 
 CREATE TABLE equations (
         id SERIAL PRIMARY KEY
-        , tgt BIGINT NOT NULL
-        , operands BIGINT[]
+        , tgt NUMERIC NOT NULL
+        , operands NUMERIC[]
 );
 
 CREATE OR REPLACE FUNCTION process_raw_data()
@@ -16,10 +16,10 @@ BEGIN
         )
         , parsed_segments AS (
                 SELECT 
-                        seg_array[1]::BIGINT AS tgt
+                        seg_array[1]::NUMERIC AS tgt
                         , ARRAY(
                                 SELECT
-                                        element::BIGINT
+                                        element::NUMERIC
                                 FROM unnest(string_to_array(seg_array[2], ' ')) AS element
                         ) AS operands
                 FROM segments
@@ -40,6 +40,7 @@ WITH RECURSIVE bfs AS (
                 , eq.operands[1] AS current_val
                 , eq.operands AS operands
                 , 1 AS idx
+                , array_length(eq.operands, 1) AS tgtidx
         FROM equations eq
 
         UNION ALL
@@ -53,6 +54,7 @@ WITH RECURSIVE bfs AS (
                 END AS current_val
                 , b.operands
                 , b.idx + 1
+                , b.tgtidx
         FROM bfs b
         CROSS JOIN unnest(ARRAY['+', '*']) AS op
         WHERE b.idx < array_length(b.operands, 1)
@@ -62,7 +64,7 @@ WITH RECURSIVE bfs AS (
                 b.*
                 , ROW_NUMBER() OVER (PARTITION BY b.id ORDER BY b.idx DESC) AS rank
         FROM bfs b
-        WHERE b.current_val = b.tgt
+        WHERE b.current_val = b.tgt AND b.idx = b.tgtidx
 )
 , part1 AS (
         SELECT
@@ -72,16 +74,60 @@ WITH RECURSIVE bfs AS (
         WHERE rank = 1
 )
 
+SELECT
+        update_solution(7, part, result)
+FROM part1;
+
+WITH RECURSIVE bfs AS (
+        SELECT
+                eq.id
+                , eq.tgt
+                , eq.operands[1] AS current_val
+                , eq.operands AS operands
+                , 1 AS idx
+                , array_length(eq.operands, 1) AS tgtidx
+                , ARRAY[]::TEXT[] as path
+        FROM equations eq
+
+        UNION ALL
+
+        SELECT
+                b.id
+                , b.tgt
+                , CASE
+                        WHEN op = '+' THEN b.current_val + b.operands[b.idx + 1]
+                        WHEN op = '*' THEN b.current_val * b.operands[b.idx + 1]
+                        WHEN op = '|' THEN CONCAT(b.current_val::TEXT, (b.operands[b.idx + 1])::TEXT)::NUMERIC
+                END AS current_val
+                , b.operands
+                , b.idx + 1
+                , b.tgtidx
+                , path || op as path
+        FROM bfs b
+        CROSS JOIN unnest(ARRAY['+', '*', '|']) AS op
+        WHERE b.idx < array_length(b.operands, 1)
+)
+, ranked AS (
+        SELECT
+                b.*
+                , ROW_NUMBER() OVER (PARTITION BY b.id ORDER BY b.idx DESC) AS rank
+        FROM bfs b
+        WHERE b.current_val = b.tgt AND b.idx = b.tgtidx
+)
+, part2 AS (
+        SELECT
+                2 AS part
+                , SUM(tgt) AS result
+        FROM ranked
+        WHERE rank = 1
+)
 
 SELECT
-        update_solution(7, results.part, results.result)
-FROM (
-        SELECT * FROM part1
-        -- UNION ALL
-        -- SELECT * FROM part2
-) AS results;
+        update_solution(7, part, result)
+FROM part2;
 
 DROP FUNCTION IF EXISTS
         process_raw_data;
 DROP TABLE IF EXISTS
-        equations;
+        raw_data
+        , equations;
