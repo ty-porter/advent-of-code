2f6b7f26e0adf9969f7f76f7598d8e03b23f093d
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-08 00:51:10 -0500

    Clean up solution

--- 2024/src/08/solution.py
+++ 2024/src/08/solution.py
@@ -1,11 +1,41 @@
 from src.prompt import Prompt
 from src.utils import Position2D
 
+
+def generate_antinodes(p1, p2, bounds, fn):
+    return fn(p1, p2, bounds) + fn(p2, p1, bounds)
+
 def inbounds(pos, bounds):
     return pos.x >= 0 and pos.x < bounds.x and pos.y >= 0 and pos.y < bounds.y
 
-def generate_antinodes(p1, p2, bounds):
-    def _generate(p1, p2, bounds):
+def find_antinodes(antennae, bounds, gen_fn):
+    antinodes = {}
+
+    for ant in antennae:
+        for i, a1pos in enumerate(antennae[ant]):
+            for j, a2pos in enumerate(antennae[ant]):
+                if i == j:
+                    continue
+
+                if ant not in antinodes:
+                    antinodes[ant] = {}
+
+                for antinode in generate_antinodes(a1pos, a2pos, bounds, gen_fn):
+                    antinodes[ant][antinode] = 1
+
+    valid_antinodes = {}
+    for antenna in antinodes:
+        for antinode in antinodes[antenna]:
+            if inbounds(antinode, bounds):
+                valid_antinodes[antinode] = 1
+
+    return len(valid_antinodes)
+
+def part_1_solution(args):
+    return find_antinodes(*args, lambda x, y, _: [x + (x - y)])
+
+def part_2_solution(args):
+    def _generate_antinodes(p1, p2, bounds):
         delta = p1 - p2
         pos = p1 + delta
 
@@ -16,57 +46,7 @@ def generate_antinodes(p1, p2, bounds):
 
         return out
 
-    return _generate(p1, p2, bounds) + _generate(p2, p1, bounds)
-
-def part_1_solution(args):
-    antennae, bounds = args
-
-    antinodes = {}
-
-    for ant in antennae:
-        for i, a1pos in enumerate(antennae[ant]):
-            for j, a2pos in enumerate(antennae[ant]):
-                if i == j:
-                    continue
-
-                if ant not in antinodes:
-                    antinodes[ant] = {}
-
-                antinodes[ant][a1pos + (a1pos - a2pos)] = 1
-                antinodes[ant][a2pos + (a2pos - a1pos)] = 1
-
-    valid_antinodes = {}
-    for antenna in antinodes:
-        for antinode in antinodes[antenna]:
-            if inbounds(antinode, bounds):
-                valid_antinodes[antinode] = 1
-
-    return len(valid_antinodes)
-
-def part_2_solution(args):
-    antennae, bounds = args
-
-    antinodes = {}
-
-    for ant in antennae:
-        for i, a1pos in enumerate(antennae[ant]):
-            for j, a2pos in enumerate(antennae[ant]):
-                if i == j:
-                    continue
-
-                if ant not in antinodes:
-                    antinodes[ant] = {}
-
-                for antinode in generate_antinodes(a1pos, a2pos, bounds):
-                    antinodes[ant][antinode] = 1
-
-    valid_antinodes = {}
-    for antenna in antinodes:
-        for antinode in antinodes[antenna]:
-            if inbounds(antinode, bounds):
-                valid_antinodes[antinode] = 1
-
-    return len(valid_antinodes)
+    return find_antinodes(*args, _generate_antinodes)
 
 
 def transform_prompt():
