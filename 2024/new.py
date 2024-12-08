f5c0ff63b0112b50ef4cb2bf5b5b96df98f1619a
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-07 20:56:44 -0500

    Platform agnostic generator

--- 2024/new.py
+++ 2024/new.py
@@ -1,9 +1,14 @@
 import os, re
 
+SOURCE_DIRECTORY = "src"
+SOLUTION_TEMPLATE = "_template.py"
+SOLUTION_NAME = "solution.py"
+PROMPT_NAME = "prompt.txt"
+
 max_solution = 0
 
-for solution_dir in os.listdir("src"):
-    if not os.path.isdir(f"src/{solution_dir}") or re.match(solution_dir, "\D"):
+for solution_dir in os.listdir(SOURCE_DIRECTORY):
+    if not os.path.isdir(f"{SOURCE_DIRECTORY}/{solution_dir}") or re.match(solution_dir, "\D"):
         continue
 
     if solution_dir == "__pycache__":
@@ -12,13 +17,15 @@ for solution_dir in os.listdir("src"):
     if int(solution_dir) > max_solution:
         max_solution = int(solution_dir)
 
-next_dir = max_solution + 1
+next_dir = str(max_solution + 1)
 
-if next_dir < 10:
-    padded_next_dir = f"0{next_dir}"
-else:
-    padded_next_dir = next_dir
+solution_dir = os.path.join(SOURCE_DIRECTORY, next_dir.rjust(2, "0"))
 
-os.system(f"mkdir src/{padded_next_dir}")
-os.system(f"touch src/{padded_next_dir}/prompt.txt")
-os.system(f"cp _template.py src/{padded_next_dir}/solution.py")
+os.mkdir(solution_dir)
+
+with open(SOLUTION_TEMPLATE, "rb") as template:
+    with open(os.path.join(solution_dir, SOLUTION_NAME), "wb") as solution:
+        solution.write(template.read())
+
+with open(os.path.join(solution_dir, PROMPT_NAME), "wb"):
+    pass
