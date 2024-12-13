80d66ecf050438e89bb3342300fd1695f02cd155
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-12 21:03:48 -0500

    Lint

--- 2024/src/12/solution.py
+++ 2024/src/12/solution.py
@@ -3,6 +3,7 @@ from src.utils import Position2D, Direction2D, CARDINAL_2D
 
 from collections import deque
 
+
 class Region:
     def __init__(self, letter, positions):
         self.letter = letter
@@ -15,7 +16,7 @@ class Region:
     @property
     def price(self):
         return self.area * self.perimeter
-    
+
     @property
     def side_price(self):
         return self.area * self.sides
@@ -58,7 +59,7 @@ class Region:
                 total += self.internal_corners(p) + self.external_corners(p)
 
         return total
-    
+
     @property
     def bounding_box(self):
         if self._bounding_box is not None:
@@ -79,7 +80,7 @@ class Region:
         self._bounding_box = (lx, ly, hx, hy)
 
         return self._bounding_box
-    
+
     def internal_corners(self, p):
         if p in self.positions:
             return 0
@@ -95,7 +96,7 @@ class Region:
 
         for triplet in corner_directions:
             total += all(p + d in self.positions for d in triplet)
-        
+
         return total
 
     def external_corners(self, p):
@@ -103,41 +104,40 @@ class Region:
             return 0
 
         neighbors = [p + d for d in CARDINAL_2D if p + d in self.positions]
-        
+
         if len(neighbors) == 0:
             return 4
 
         if len(neighbors) == 1:
             return 2
-        
+
         if len(neighbors) == 2:
             vec = neighbors[0] - neighbors[1]
 
             if abs(vec.x) > 1 or abs(vec.y) > 1:
                 return 0
-            
+
             return 1
 
         return 0
 
-
     @staticmethod
     def find(grid, x, y):
         target = grid[y][x]
-        visited = { Position2D(x, y): 1 }
+        visited = {Position2D(x, y): 1}
         found = {}
         queue = deque([Position2D(x, y)])
 
         while queue:
             position = queue.popleft()
             visited[position] = 1
-            
+
             if grid[position.y][position.x] == target:
                 found[position] = 1
 
             for d in CARDINAL_2D:
                 p = position + d
-                
+
                 y_in_bound = p.y >= 0 and p.y < len(grid)
                 x_in_bound = p.x >= 0 and p.x < len(grid[0])
 
@@ -153,11 +153,14 @@ class Region:
 
         return Region(target, found)
 
+
 def part_1_solution(regions):
     return sum(region.price for region in regions)
 
+
 def part_2_solution(regions):
     return sum(region.side_price for region in regions)
+
 
 def transform_prompt():
     grid = Prompt.read_to_grid(__file__)
