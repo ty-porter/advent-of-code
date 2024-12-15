6d1f1b6a2bd69aaaaffadeb40187da8a5449d7da
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-15 00:05:59 -0500

    SQL 14p2

--- 2024-sql/solutions/12/solution.sql
+++ 2024-sql/solutions/12/solution.sql
@@ -83,6 +83,124 @@ $$ LANGUAGE plpgsql;
 
 SELECT define_regions();
 
+CREATE OR REPLACE FUNCTION find_sides(rid INT)
+RETURNS INT AS $$
+DECLARE
+        -- bounding box
+        x1 INT;
+        y1 INT;
+        x2 INT;
+        y2 INT;
+
+        -- iterators
+        xi INT;
+        yi INT;
+
+        -- neighbor counting
+        in_region BOOLEAN;
+        neighbors BIT(8);
+        cardinal_neighbors BIT(4);
+        n_count INT;
+
+        -- corner counting
+        cell_corners INT;
+        corners INT := 0;
+BEGIN
+        -- Define the region's bounding box
+        SELECT min(x), min(y), max(x), max(y) INTO x1, y1, x2, y2 FROM grid WHERE region_id = rid;
+
+        -- Iterate over cells in the bounding box and check for corners
+        FOR yi IN y1..y2
+        LOOP
+                FOR xi IN x1..x2
+                LOOP
+                        SELECT true INTO in_region FROM grid WHERE x = xi AND y = yi AND region_id = rid;
+
+                        cell_corners := 0;
+
+                        WITH adjacent_cells AS (
+                                SELECT
+                                        ord, x, y
+                                FROM (
+                                        VALUES
+                                        (1, xi    , yi - 1),  -- Up
+                                        (2, xi + 1, yi    ),  -- Right
+                                        (3, xi    , yi + 1),  -- Down
+                                        (4, xi - 1, yi    ),  -- Left
+                                        (5, xi + 1, yi - 1),  -- Up-Right
+                                        (6, xi + 1, yi + 1),  -- Down-Right
+                                        (7, xi - 1, yi + 1),  -- Down-Left
+                                        (8, xi - 1, yi - 1)   -- Up-Left
+                                ) AS adjacent(ord, x, y)
+                        )
+                        , ordered_cells AS ( 
+                                SELECT
+                                        ac.*
+                                        , CASE
+                                                WHEN g.id IS NOT NULL THEN '1'
+                                                ELSE '0'
+                                        END AS val
+                                FROM adjacent_cells ac
+                                LEFT JOIN grid g
+                                        ON g.x = ac.x AND g.y = ac.y AND g.region_id = rid
+                                ORDER BY ac.ord ASC
+                        )
+                        SELECT
+                                string_agg(oc.val, '') AS bitmask
+                        INTO
+                                neighbors
+                        FROM ordered_cells oc;
+
+                        IF in_region THEN -- Handle external corners
+                                -- Casting to BIT(4) from BIT(8) preserves the highest 4 bits??
+                                cardinal_neighbors := neighbors::BIT(4);
+
+                                n_count := 0;
+
+                                FOR n IN 0..3
+                                LOOP
+                                        n_count := n_count + ((cardinal_neighbors::INT >> n) & 1)::INT;
+                                END LOOP;
+
+                                IF n_count = 0 THEN
+                                        cell_corners := 4;
+                                ELSIF n_count = 1 THEN
+                                        cell_corners := 2;
+                                ELSIF n_count = 2 THEN
+                                        -- Use the bitmask to detect which permutations of neighbors we have
+                                        IF cardinal_neighbors = ANY(ARRAY[3::BIT(4), 6::BIT(4), 9::BIT(4), 12::BIT(4)]) THEN
+                                                cell_corners := 1;
+                                        END IF;
+                                END IF;
+
+                        ELSE -- Handle internal corners
+                                -- More bitmask tricks. Looking for 3 neighbors in an L-shape (i.e. up, right, up-right)
+                                -- Bottom left (left / down / down-left)
+                                IF neighbors & 50::BIT(8) = 50::BIT(8) THEN
+                                        cell_corners := cell_corners + 1;
+                                END IF;
+                                -- Bottom right (right / down / down-right)
+                                IF neighbors & 100::BIT(8) = 100::BIT(8) THEN
+                                        cell_corners := cell_corners + 1;
+                                END IF;
+                                -- Top left (left / up / down-left)
+                                IF neighbors & 145::BIT(8) = 145::BIT(8) THEN
+                                        cell_corners := cell_corners + 1;
+                                END IF;
+                                -- Top right (right / up / down-right)
+                                IF neighbors & 200::BIT(8) = 200::BIT(8) THEN
+                                        cell_corners := cell_corners + 1;
+                                END IF;
+                        END IF;
+
+                        corners := corners + cell_corners;
+                END LOOP;
+        END LOOP;
+
+        RETURN corners;
+END;
+$$ LANGUAGE plpgsql;
+
 WITH area AS (
         SELECT
                 region_id
@@ -108,13 +226,32 @@ WITH area AS (
         FROM perimeters
         GROUP BY region_id
 )
+, sides AS (
+        WITH distinct_regions AS (
+                SELECT
+                        DISTINCT region_id
+                FROM grid
+        )
+        SELECT
+                region_id
+                , find_sides(region_id) AS sides
+        FROM distinct_regions
+)
 , cost AS (
         SELECT
                 r.region_id
+                , r.c
                 , (SELECT a.area FROM area a WHERE a.region_id = r.region_id) AS area
-                , (SELECt p.perimeter FROM perimeter p WHERE p.region_id = r.region_id) AS perimeter
+                , (SELECT p.perimeter FROM perimeter p WHERE p.region_id = r.region_id) AS perimeter
         FROM grid r
-        GROUP BY r.region_id
+        GROUP BY r.region_id, r.c
+)
+, cost_with_sides AS (
+        SELECT
+                c.*
+                , s.sides
+        FROM cost c
+        INNER JOIN sides s ON c.region_id = s.region_id
 )
 , part1 AS (
         SELECT
@@ -122,20 +259,24 @@ WITH area AS (
                 , sum(area * perimeter) AS result
         FROM cost
 )
--- , part2 AS (
-        
--- )
+, part2 AS (
+        SELECT
+                2 AS part
+                , sum(area * sides) AS result
+        FROM cost_with_sides
+)
 
 SELECT
         update_solution(12, results.part, results.result)
 FROM (
         SELECT * FROM part1
-        -- UNION ALL
-        -- SELECT * FROM part2
+        UNION ALL
+        SELECT * FROM part2
 ) AS results;
 
 DROP FUNCTION
-        process_raw_data,
-        define_regions;
+        process_raw_data
+        , define_regions
+        , find_sides;
 DROP TABLE
         grid;
