fbc9b4922209311a820bdf0d7812a966b342f471
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-15 11:05:14 -0500

    Remove debug

--- 2024/src/15/solution.py
+++ 2024/src/15/solution.py
@@ -1,7 +1,5 @@
 from src.prompt import Prompt
-from src.utils import Position2D, Direction2D, print_2d_grid
-
-import copy
+from src.utils import Position2D, Direction2D
 
 
 DIRECTIONS = {
@@ -10,29 +8,6 @@ DIRECTIONS = {
     'v': Direction2D.DOWN(),
     '<': Direction2D.LEFT()
 }
-
-GRID1 = [[c for c in row] for row in [
-    '########',
-    '#......#',
-    '##.....#',
-    '#......#',
-    '#.#....#',
-    '#......#',
-    '#......#',
-    '########'
-]]
-GRID2 = [[c for c in row] for row in [
-    '##########',
-    '#........#',
-    '#........#',
-    '#........#',
-    '#........#',
-    '#.#......#',
-    '#........#',
-    '#........#',
-    '#........#',
-    '##########',
-]]
 
 
 def furthest_movable(position, direction, obstacles, boxes):
@@ -49,43 +24,15 @@ def part_1_solution(args):
 
     for instruction in instructions:
         direction = DIRECTIONS[instruction]
-        # print(bot, 'moving', direction, f'({instruction})', "to", bot + direction)
 
         movable = furthest_movable(bot, direction, obstacles, boxes)
         
         if movable is not None:
             bot = bot + direction
             if bot in boxes:
-                # breakpoint()
-                # print(f"removing bot {bot} from boxes")
                 del boxes[bot]
 
-            # if movable in boxes:
                 boxes[movable + direction] = 1
-
-        # BEGIN DEBUG
-        # print("boxes:", boxes)
-        # print("obstacles:", obstacles)
-        
-        # grid = copy.deepcopy(GRID2)
-
-        # for box in boxes:
-        #     try:
-        #         grid[box.y][box.x] = 'O'
-        #     except:
-        #         # breakpoint()
-        #         pass
-
-
-
-        # grid[bot.y][bot.x] = '@'
-
-        # print_2d_grid(grid)
-        # print()
-
-        # if instruction == '>':
-        #     breakpoint()
-         # END DEBUG
 
     return sum(box.y * 100 + box.x for box in boxes)
 
@@ -112,9 +59,5 @@ def transform_prompt():
                 boxes[Position2D(x, y)] = 1
             elif col == '@':
                 bot = Position2D(x, y)
-
-    # print(instructions)
-
-    # instructions = lines[-1]
 
     return obstacles, boxes, bot, instructions
