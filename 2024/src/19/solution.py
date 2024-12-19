f5e29959b34206592fe6fe0130d46282ffde7ca2
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-19 01:03:30 -0500

    Python Day 19p1

---
+++ 2024/src/19/solution.py
@@ -0,0 +1,50 @@
+from src.utils.prompt import Prompt
+from src.utils import skippable
+from functools import cache
+
+from queue import PriorityQueue
+
+
+def part_1_solution(args):
+    patterns, designs = args
+
+    total = 0
+    for design in designs:
+        found = 0
+        pq = PriorityQueue()
+        for pattern in patterns:
+            pq.put((len(design), pattern, design))
+
+
+        while not pq.empty():
+            sz, pat, des = pq.get()
+
+            if pat == des:
+                found = 1
+                break
+
+            if pat != des[:len(pat)] or len(pat) > sz:
+                continue
+
+            next_des = des[len(pat):]
+            next_sz = len(next_des)
+
+            for next_pat in patterns:
+                if next_pat != next_des[:len(next_pat)] or len(next_pat) > next_sz:
+                    continue
+
+                pq.put((next_sz, next_pat, next_des))
+
+        total += found
+
+    return total
+
+
+def part_2_solution(args):
+    patterns, designs = args
+
+def transform_prompt():
+    patterns, _, *design = Prompt.read_to_list(__file__)
+    patterns = [pattern for pattern in patterns.split(', ')]
+
+    return patterns, design
