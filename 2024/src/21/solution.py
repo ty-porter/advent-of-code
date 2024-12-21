7d4ad03012f94009b6caf52ee82e353d76077e63
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-21 15:29:41 -0500

    21p2

--- 2024/src/21/solution.py
+++ 2024/src/21/solution.py
@@ -1,7 +1,7 @@
 from src.utils.prompt import Prompt
-from src.utils import skippable
 
 from functools import cache
+from collections import Counter
 
 KP_PATH = {
     ('A', '<'): 'v<<',
@@ -28,7 +28,7 @@ KP_PATH = {
     ('A', '1'): '^<<',
     ('A', '3'): '^',
     ('A', '4'): '^^<<',
-    ('A', '5'): '^^<',
+    ('A', '5'): '<^^',
     ('A', '6'): '^^',
     ('A', '7'): '^^^<<',
     ('A', '9'): '^^^',
@@ -44,7 +44,7 @@ KP_PATH = {
     ('3', '5'): '<^',
     ('3', '7'): '<<^^',
     ('3', '8'): '<^^',
-    ('4', '0'): 'vv>',
+    ('4', '0'): '>vv',
     ('4', '5'): '>',
     ('4', '6'): '>>',
     ('4', '8'): '^>',
@@ -56,12 +56,12 @@ KP_PATH = {
     ('6', '7'): '<<^',
     ('7', '1'): 'vv',
     ('7', '6'): 'v>>',
-    ('7', '6'): 'v>>',
     ('7', '8'): '>',
     ('7', '9'): '>>',
     ('8', '0'): 'vvv',
     ('8', '2'): 'vv',
     ('8', '5'): 'v',
+    ('9', 'A'): 'vvv',
     ('9', '8'): '<'
 }
 
@@ -77,27 +77,35 @@ def encode(seq):
         out.append(KP_PATH[(current, button)] + 'A')
         current = button
 
-    return out
+    return "".join(out)
 
 def complexity(codes, robots):
     total = 0
+
     for code in codes:
-        sequences = [code]
+        sequences = Counter()
+        sequences[code] += 1
+
         for _ in range(robots):
-            next_sequences = []
-            for seq in sequences:
-                next_sequences.extend(encode(seq))
+            seq2 = Counter()
 
-            sequences = next_sequences
+            for sequence, amt in sequences.items():
+                encoding = encode(sequence)
 
-        total += len("".join(sequences)) * int(code[:3])
+                parts = encoding.split("A")
+
+                for part in parts[:-1]:                   
+                    seq2[part + 'A'] += amt
+
+            sequences = seq2
+
+        total += sum(len(k) * v for k,v in sequences.items()) * int(code[:3])
 
     return total
 
 def part_1_solution(codes):
     return complexity(codes, 3)
 
-@skippable("21p2")
 def part_2_solution(codes):
     return complexity(codes, 26)
 
