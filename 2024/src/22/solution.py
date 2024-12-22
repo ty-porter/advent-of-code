957535e033eac61a1f54b14fc7e8ca018e2964a3
Tyler Porter <tyler.b.porter@gmail.com>
2024-12-22 00:37:11 -0500

    Python 22p2

--- 2024/src/22/solution.py
+++ 2024/src/22/solution.py
@@ -1,23 +1,48 @@
 from src.utils.prompt import Prompt
 
+from collections import Counter
 
-def part_1_solution(values):
-    s = 0
+def generate_secret_numbers(values):
+    total = 0
+    sequences = Counter()
+
     for value in values:
         v = int(value)
+        o = None
+        buf = []
+        vseq = Counter()
 
-        for _ in range(2000):
+        for i in range(2000):
+            o = v
             v = (v ^ (v * 64)) % 16777216
             v = (v ^ (v // 32)) % 16777216
             v = (v ^ (v * 2048)) % 16777216
 
-        s += v
+            if i >= 0:
+                buf.append((v % 10) - (o % 10))
 
-    return s
+            if len(buf) > 4:
+                buf.pop(0)
+
+            if len(buf) == 4:
+                if tuple(buf) not in vseq:
+                    vseq[tuple(buf)] += v % 10
+
+        total += v
+
+        for seq, val in vseq.items():
+            sequences[seq] += val
+
+    return total, sequences
+
+def part_1_solution(values):
+    return generate_secret_numbers(values)[0]
 
 
 def part_2_solution(values):
-    return
+    _, sequences = generate_secret_numbers(values)
+
+    return sequences[max(sequences, key=sequences.get)]
 
 
 def transform_prompt():
